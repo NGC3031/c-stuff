21b1dd14382ecee34aa22bc0eeac927671901046
NGC3031 <scott.d@ieee.org>
2019-04-02 02:38:29 +1100

    wordPerLine.c

---
+++ wordPerLine.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* prints each word on a seperate line  */
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+            putchar('\n');
+        else putchar(c);
+    }
+}
