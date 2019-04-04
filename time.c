1204390f667fda1caa2e759982685a606cf7d534
NGC3031 <scott.d@ieee.org>
2019-04-05 02:17:31 +1100

    time.c

---
+++ time.c
@@ -0,0 +1,11 @@
+/* return the largest system time */
+
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+    time_t biggest = 0x7FFFFFFF;
+    printf("biggest = %s \n", asctime(gmtime(&biggest)));
+    //printf("biggest = %s \n", ctime(&biggest));
+    return 0;
+}
